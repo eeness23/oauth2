7cbab07c196419ea29b0cd51d9048f0b2ba69bcd
Enes Demirbas <enesdemirbas95@gmail.com>
2020-07-31 14:18:09 +0300

    develop client credentials

---
+++ auth-server/src/main/java/com/enes/oauth2/authserver/config/AuthServerConfig.java
@@ -0,0 +1,39 @@
+package com.enes.oauth2.authserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("testClient1")
+                .authorizedGrantTypes("password", "authorization_code")
+                .secret(passwordEncoder.encode("enes"))
+                .scopes("user_info")
+                .redirectUris("http://localhost:8080/login/oauth2/code/custom")
+                .autoApprove(false)
+                .and()
+
+                .withClient("testClient2")
+                .authorizedGrantTypes("password", "authorization_code", "client_credentials")
+                .secret(passwordEncoder.encode("enes"))
+                .scopes("user_info")
+                .redirectUris("http://localhost:8080/login/oauth2/code/custom")
+                .autoApprove(false);
+
+
+    }
+}
